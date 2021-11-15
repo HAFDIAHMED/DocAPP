84dd216d14583f28c3c905e3257fddbd49263581
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-15 12:29:18 +0100

    DOCUMENTATION

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -8,7 +8,7 @@ import { CharacterStoreModel } from "../character-store/character-store"
 // prettier-ignore
 export const RootStoreModel = types.model("RootStore").props({
   characterStore: types.optional(CharacterStoreModel, {} as any),
-  //ProfileStore : types.optional(ProfileModel,{} as any),
+  ProfileStore : types.optional(ProfileModel,{} as any),
 })
 
 /**
