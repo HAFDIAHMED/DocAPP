eb87e1dc244af8f24004566fde187ead1ed039e9
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-15 14:57:34 +0100

    DOCUMENTATION

--- app/screens/authentification/signin/signin-screen.tsx
+++ app/screens/authentification/signin/signin-screen.tsx
@@ -24,7 +24,6 @@ export const SigninScreen = observer(function SigninScreen() {
     profilePassword:""
   })
   useEffect(()=>{
-      console.log("hello")
   })
   return (
     <Screen style={ROOT} preset="scroll">
