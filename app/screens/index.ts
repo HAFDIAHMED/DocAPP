25bb2d7a6836f0f74da8d9d29ea1ca6c103b405c
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-15 12:39:24 +0100

    DOCUMENTATION

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -4,3 +4,5 @@ export * from "./demo/demo-list-screen"
 export * from "./error/error-boundary"
 // export other screens here
 export * from "./profile/profile-screen"
+export * from "./signin/signin-screen"
+export * from "./signup/signup-screen"
