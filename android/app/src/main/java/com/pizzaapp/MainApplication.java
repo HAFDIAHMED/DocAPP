93c0e2bc5efae93926872e59d9145c1de925367b
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-17 14:45:53 +0100

    DOCUMENTATION

--- android/app/src/main/java/com/pizzaapp/MainApplication.java
+++ android/app/src/main/java/com/pizzaapp/MainApplication.java
@@ -7,6 +7,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import org.wonday.pdf.RCTPdfView;
 import com.ReactNativeBlobUtil.ReactNativeBlobUtilPackage;
 import com.facebook.react.ReactInstanceManager;
