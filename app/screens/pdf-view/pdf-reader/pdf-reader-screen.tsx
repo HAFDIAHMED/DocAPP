dc19251d8df79e5312ebf2cc1b4cb84ad4d52778
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-17 16:38:23 +0100

    DOCUMENTATION

--- app/screens/pdf-view/pdf-reader/pdf-reader-screen.tsx
+++ app/screens/pdf-view/pdf-reader/pdf-reader-screen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react"
 import { observer } from "mobx-react-lite"
-import { Dimensions, View, ViewStyle } from "react-native"
+import { Dimensions, PermissionsAndroid, Platform, View, ViewStyle } from "react-native"
 import { Screen, Text } from "../../../components"
 import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
@@ -46,7 +46,82 @@ export const PdfReaderScreen = observer(function PdfReaderScreen() {
   }
  useEffect(()=>{
    download_pdf();
- })
+ });
+ const REMOTE_IMAGE_PATH =
+ 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/gift.png'
+const checkPermission = async () => {
+ 
+ // Function to check the platform
+ // If iOS then start downloading
+ // If Android then ask for permission
+
+ if (Platform.OS === 'ios') {
+   downloadImage();
+ } else {
+   try {
+     const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+       
+     );
+     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+       // Once user grant the permission start downloading
+       console.log('Storage Permission Granted.');
+       downloadImage();
+     } else {
+       // If permission denied then show alert
+       alert('Storage Permission Not Granted');
+     }
+   } catch (err) {
+     // To handle permission related exception
+     console.warn(err);
+   }
+ }
+};
+
+const downloadImage = () => {
+ // Main function to download the image
+ 
+ // To add the time suffix in filename
+ let date = new Date();
+ // Image URL which we want to download
+ let image_URL = REMOTE_IMAGE_PATH;    
+ // Getting the extention of the file
+ let ext = getExtention(image_URL);
+    ext = '.' + ext[0];
+ // Get config and fs from RNFetchBlob
+ // config: To pass the downloading related options
+ // fs: Directory path where we want our image to download
+ const { config, fs } = RNFetchBlob;
+ let PictureDir = fs.dirs.PictureDir;
+ let options = {
+   fileCache: true,
+   addAndroidDownloads: {
+     // Related to the Android only
+     useDownloadManager: true,
+     notification: true,
+     path:
+       PictureDir +
+       '/image_' + 
+       Math.floor(date.getTime() + date.getSeconds() / 2) +
+       ext,
+     description: 'Image',
+   },
+ };
+ config(options)
+   .fetch('GET', image_URL)
+   .then(res => {
+     // Showing alert after successful downloading
+     console.log('res -> ', JSON.stringify(res));
+     alert('Image Downloaded Successfully.');
+   });
+};
+
+const getExtention = filename => {
+ // To get the file extension
+ return /[.]/.exec(filename) ?
+          /[^.]+$/.exec(filename) : undefined;
+};
+
+
   return (
     <Screen style={ROOT} preset="scroll">
       <View style={TOOLS_PDF}>
