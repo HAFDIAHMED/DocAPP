364aef8fb0fd97e92cbe9a51690bf8e829494ffd
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-19 12:59:05 +0100

    DOCUMENTATION

--- app/screens/map-view/map-geo/map-geo-screen.tsx
+++ app/screens/map-view/map-geo/map-geo-screen.tsx
@@ -11,7 +11,7 @@ import { TextInput } from "react-native-gesture-handler"
 import { Switch } from "react-native-elements/dist/switch/switch"
 import SwitchWithIcons from "react-native-switch-with-icons";
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import {Night} from "./mapStyles_variation" ;
 export const MapGeoScreen = observer(function MapGeoScreen() {
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
@@ -526,7 +526,7 @@ return(
 
 provider={PROVIDER_GOOGLE}
 style={styles.map}
-customMapStyle={styleChange ? mapStyle_Custom :mapStyle_Night}
+customMapStyle={styleChange ? mapStyle_Custom :Night}
 region={{
  
  latitude: 33.56729634144786,
@@ -574,7 +574,7 @@ image={{uri : "https://www.pngkit.com/bigpic/u2q8a9e6y3w7a9u2/"}}
       <View style={ROW_STYLES_MAP}>
       <Icon name ="home"  color="red" size={30}/>
       <Icon name ="home"  color="red" size={30}/>
-      <Button  text="change" onPress={()=>{setStyleChange(!styleChange);}}/>
+      <Button  text="change" onPress={()=>{setStyleChange(!styleChange);console.log(Night)}}/>
 
       </View>
            
