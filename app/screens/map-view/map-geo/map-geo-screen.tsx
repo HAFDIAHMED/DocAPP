23605fe28d52f68a382acd923f1063cc60e1fdfc
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-19 15:13:58 +0100

    DOCUMENTATION

--- app/screens/map-view/map-geo/map-geo-screen.tsx
+++ app/screens/map-view/map-geo/map-geo-screen.tsx
@@ -9,7 +9,7 @@ import metrics from "../../../theme/metrics"
 import MapView, { Callout, Marker, Polygon, PROVIDER_GOOGLE } from "react-native-maps"
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler"
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {ahmed,hamid} from "../map-geo/map_many/mapStyles_variation"
+import {Standard,Night} from "../map-geo/map_many/mapStyles_variation"
 export const MapGeoScreen = observer(function MapGeoScreen() {
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
@@ -563,11 +563,11 @@ image={{uri : "https://www.pngkit.com/bigpic/u2q8a9e6y3w7a9u2/"}}
         
       <View style={ROW_STYLES_MAP}>
 
-      <TouchableOpacity style={MAP_STYLE_PAGE} onPress={()=>setMapStyles(mapStyle_Night)}>
+      <TouchableOpacity style={MAP_STYLE_PAGE} onPress={()=>setMapStyles(Night)}>
       <Icon  name ="home"  color="black" size={30} />
       <Text style={MAP_STYLE_TEXT}>Night</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={MAP_STYLE_PAGE} onPress={()=>setMapStyles(mapStyle_Custom)}>
+      <TouchableOpacity style={MAP_STYLE_PAGE} onPress={()=>setMapStyles(Standard)}>
       <Icon name ="home"  color="orange" size={30} />
       <Text style={MAP_STYLE_TEXT}>Standard</Text>
 
@@ -575,13 +575,12 @@ image={{uri : "https://www.pngkit.com/bigpic/u2q8a9e6y3w7a9u2/"}}
 
       
       </View>
-      <Text>zzz</Text>
            
               </Callout>
     );
   }
   useEffect(()=>{
-    console.log(hamid)
+    //console.log(Standard)
   })
   return (
     <Screen style={ROOT} preset="scroll">
