081316b15e410db0e5fb94d87ba899d172bb9847
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-19 17:32:46 +0100

    DOCUMENTATION

--- app/screens/map-view/map-geo/map-geo-screen.tsx
+++ app/screens/map-view/map-geo/map-geo-screen.tsx
@@ -127,69 +127,77 @@ customMapStyle={mapStyles}
               </Callout>
     );
   }
+  const AUTOCOMPLETE_SEARCH=()=>{
+    return (
+        <View>
+          <GooglePlacesAutocomplete
+
+placeholder='Search'
+onPress={(data, details = null) => {
+  // 'details' is provided when fetchDetails = true
+  console.log(data, details);
+}}
+query={{
+  key: 'AIzaSyC2vu-vZH1nysQT7xXxZ4J-H2CbDx0iDq0',
+  language: 'en',
+}}
+styles={
+  {
+    container: {
+      flex: 1,
+    },
+    textInputContainer: {
+      flexDirection: 'row',
+    },
+    textInput: {
+      backgroundColor: '#FFFFFF',
+      height: 44,
+      borderRadius: 5,
+      paddingVertical: 5,
+      paddingHorizontal: 10,
+      fontSize: 15,
+      flex: 1,
+    },
+    poweredContainer: {
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      borderBottomRightRadius: 5,
+      borderBottomLeftRadius: 5,
+      borderColor: '#c8c7cc',
+      borderTopWidth: 0.5,
+    },
+    powered: {},
+    listView: {},
+    row: {
+      backgroundColor: '#FFFFFF',
+      padding: 13,
+      height: 44,
+      flexDirection: 'row',
+    },
+    separator: {
+      height: 0.5,
+      backgroundColor: '#c8c7cc',
+    },
+    description: {},
+    loader: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      height: 20,
+    },
+  }
+}
+/>
+        </View>
+    );
+  }
   useEffect(()=>{
     //console.log(Standard)
   })
   return (
     <Screen style={ROOT} >
-   
-   <GooglePlacesAutocomplete
-
-      placeholder='Search'
-      onPress={(data, details = null) => {
-        // 'details' is provided when fetchDetails = true
-        console.log(data, details);
-      }}
-      query={{
-        key: 'AIzaSyC2vu-vZH1nysQT7xXxZ4J-H2CbDx0iDq0',
-        language: 'en',
-      }}
-      styles={
-        {
-          container: {
-            flex: 1,
-          },
-          textInputContainer: {
-            flexDirection: 'row',
-          },
-          textInput: {
-            backgroundColor: '#FFFFFF',
-            height: 44,
-            borderRadius: 5,
-            paddingVertical: 5,
-            paddingHorizontal: 10,
-            fontSize: 15,
-            flex: 1,
-          },
-          poweredContainer: {
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            borderBottomRightRadius: 5,
-            borderBottomLeftRadius: 5,
-            borderColor: '#c8c7cc',
-            borderTopWidth: 0.5,
-          },
-          powered: {},
-          listView: {},
-          row: {
-            backgroundColor: '#FFFFFF',
-            padding: 13,
-            height: 44,
-            flexDirection: 'row',
-          },
-          separator: {
-            height: 0.5,
-            backgroundColor: '#c8c7cc',
-          },
-          description: {},
-          loader: {
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            height: 20,
-          },
-        }
-      }
-    />
+   <Map_View/>
+  <MAPS/>
+   <AUTOCOMPLETE_SEARCH/>
     </Screen>
   )
 })
@@ -197,7 +205,7 @@ customMapStyle={mapStyles}
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
-  alignItems:'center',
+  //alignItems:'center',
   //justifyContent:'center',
 }
 
