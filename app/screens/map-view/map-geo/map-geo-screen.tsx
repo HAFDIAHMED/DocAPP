a13ec519efbfaf64572c734a914c908a63c8dfcd
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-19 17:25:19 +0100

    DOCUMENTATION

--- app/screens/map-view/map-geo/map-geo-screen.tsx
+++ app/screens/map-view/map-geo/map-geo-screen.tsx
@@ -145,6 +145,51 @@ customMapStyle={mapStyles}
         key: 'AIzaSyC2vu-vZH1nysQT7xXxZ4J-H2CbDx0iDq0',
         language: 'en',
       }}
+      styles={
+        {
+          container: {
+            flex: 1,
+          },
+          textInputContainer: {
+            flexDirection: 'row',
+          },
+          textInput: {
+            backgroundColor: '#FFFFFF',
+            height: 44,
+            borderRadius: 5,
+            paddingVertical: 5,
+            paddingHorizontal: 10,
+            fontSize: 15,
+            flex: 1,
+          },
+          poweredContainer: {
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            borderBottomRightRadius: 5,
+            borderBottomLeftRadius: 5,
+            borderColor: '#c8c7cc',
+            borderTopWidth: 0.5,
+          },
+          powered: {},
+          listView: {},
+          row: {
+            backgroundColor: '#FFFFFF',
+            padding: 13,
+            height: 44,
+            flexDirection: 'row',
+          },
+          separator: {
+            height: 0.5,
+            backgroundColor: '#c8c7cc',
+          },
+          description: {},
+          loader: {
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            height: 20,
+          },
+        }
+      }
     />
     </Screen>
   )
