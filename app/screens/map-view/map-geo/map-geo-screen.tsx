3afbb2fec9b56fdd42efe26c32d9071bd1673bae
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-19 11:38:43 +0100

    DOCUMENTATION

--- app/screens/map-view/map-geo/map-geo-screen.tsx
+++ app/screens/map-view/map-geo/map-geo-screen.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { StyleSheet, TextStyle, ViewStyle } from "react-native"
+import { StyleSheet, TextStyle, View, ViewStyle } from "react-native"
 import { Screen, Button,Text } from "../../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
@@ -516,27 +516,32 @@ export const MapGeoScreen = observer(function MapGeoScreen() {
   ]
   const Map_View =()=>{
 return(
-  <MapView
+  <View>
+    <MapView
 
-       provider={PROVIDER_GOOGLE}
-      style={styles.map}
-      customMapStyle={mapStyle_Night}
-      region={{
-        
-        latitude: 33.56729634144786,
-        longitude: -7.6277178270787225,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}       showsUserLocation={true} >
-       <Marker coordinate={{
-         latitude: 33.56729634144786,
-         longitude: -7.6277178270787225,
-       }} 
-       image={{uri : "https://www.pngkit.com/bigpic/u2q8a9e6y3w7a9u2/"}}
-       
-       />
-      
-       </MapView>
+provider={PROVIDER_GOOGLE}
+style={styles.map}
+customMapStyle={mapStyle_Night}
+region={{
+ 
+ latitude: 33.56729634144786,
+ longitude: -7.6277178270787225,
+ latitudeDelta: 0.015,
+ longitudeDelta: 0.0121,
+}}       showsUserLocation={true} >
+<Marker coordinate={{
+  latitude: 33.56729634144786,
+  longitude: -7.6277178270787225,
+}} 
+image={{uri : "https://www.pngkit.com/bigpic/u2q8a9e6y3w7a9u2/"}}
+
+/>
+
+</MapView>
+<Callout>
+        <Text>heloo</Text>
+      </Callout>
+  </View>
 );
       }
  const Polygone_View =()=>{
@@ -558,9 +563,7 @@ return(
   return (
     <Screen style={ROOT} preset="scroll">
    <Map_View />
-      <Callout>
-        <Text>heloo</Text>
-      </Callout>
+      
     </Screen>
   )
 })
