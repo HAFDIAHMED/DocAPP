e0d6a456166eb03876cf46dc78016c9aa486ad20
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-19 15:38:51 +0100

    DOCUMENTATION

--- app/screens/map-view/map-geo/map-geo-screen.tsx
+++ app/screens/map-view/map-geo/map-geo-screen.tsx
@@ -71,16 +71,17 @@ image={{uri : "https://www.pngkit.com/bigpic/u2q8a9e6y3w7a9u2/"}}
       <Icon  name ="moon"  color="black" size={30}  />
       <Text style={MAP_STYLE_TEXT}>Night</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={MAP_STYLE_PAGE} onPress={()=>setMapStyles(Standard)}>
-      <Icon name ="home"  color="orange" size={30} />
-      <Text style={MAP_STYLE_TEXT}>Standard</Text>
-
-      </TouchableOpacity>
       <TouchableOpacity style={MAP_STYLE_PAGE} onPress={()=>setMapStyles(Retro)}>
       <Icon name ="home"  color="yellow" size={30} />
       <Text style={MAP_STYLE_TEXT}>Retro</Text>
 
       </TouchableOpacity>
+      <TouchableOpacity style={MAP_STYLE_PAGE} onPress={()=>setMapStyles(Standard)}>
+      <Icon name ="sun"  color="orange" size={30} />
+      <Text style={MAP_STYLE_TEXT}>Standard</Text>
+
+      </TouchableOpacity>
+      
 
       
       </View>
