1a1b5745e780062bfdef9588bfc31a239e71214d
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-19 09:46:50 +0100

    DOCUMENTATION

--- app/screens/map-view/map-geo/map-geo-screen.tsx
+++ app/screens/map-view/map-geo/map-geo-screen.tsx
@@ -21,12 +21,7 @@ export const MapGeoScreen = observer(function MapGeoScreen() {
    <MapView
        provider={PROVIDER_GOOGLE}
       style={styles.map}
-       initialRegion={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
+      
        showUserLocation={true} >
        <Marker coordinate={{
          latitude: 37.78825,
