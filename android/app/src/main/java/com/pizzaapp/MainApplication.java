e7a1ca4e263d3241cb6a8162e21ff65c45e2d025
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-22 15:28:03 +0100

    DOCUMENTATION

--- android/app/src/main/java/com/pizzaapp/MainApplication.java
+++ android/app/src/main/java/com/pizzaapp/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import org.reactnative.camera.RNCameraPackage;
 import org.reactnative.camera.RNCameraPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.horcrux.svg.SvgPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
