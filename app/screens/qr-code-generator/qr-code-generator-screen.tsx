23cfbfa53b96b515e510afba9178c8d28662b401
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-22 14:14:19 +0100

    DOCUMENTATION

--- app/screens/qr-code-generator/qr-code-generator-screen.tsx
+++ app/screens/qr-code-generator/qr-code-generator-screen.tsx
@@ -1,16 +1,13 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { View, ViewStyle } from "react-native"
 import { Screen, Text } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
 import QRCode from 'react-native-qrcode-svg';
 
-const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
-  flex: 1,
-}
+
 
 export const QrCodeGeneratorScreen = observer(function QrCodeGeneratorScreen() {
   // Pull in one of our MST stores
@@ -22,10 +19,24 @@ export const QrCodeGeneratorScreen = observer(function QrCodeGeneratorScreen() {
     <Screen style={ROOT} preset="scroll">
       
       <Text preset="header" text="Qr Code"  style={{alignSelf:'center'}}/>
+      <View style={QR_STYLE}>
       <QRCode
       value="https://www.npmjs.com/package/react-native-qrcode-svg"
     />
+      </View>
+      
 
     </Screen>
   )
 })
+
+const ROOT: ViewStyle = {
+  backgroundColor: color.palette.black,
+  flex: 1,
+  justifyContent:'center',
+  alignItems:'center',
+}
+const QR_STYLE :ViewStyle={
+  alignSelf:'center',
+ 
+}
