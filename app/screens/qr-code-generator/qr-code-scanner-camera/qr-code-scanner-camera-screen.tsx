1154e591994c3708564d7efd93e4b463031876ef
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-22 14:51:27 +0100

    DOCUMENTATION

--- app/screens/qr-code-generator/qr-code-scanner-camera/qr-code-scanner-camera-screen.tsx
+++ app/screens/qr-code-generator/qr-code-scanner-camera/qr-code-scanner-camera-screen.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
-import { Screen, Text } from "../../components"
+import { Screen, Text } from "../../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
-import { color } from "../../theme"
+import { color } from "../../../theme"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
@@ -19,7 +19,7 @@ export const QrCodeScannerCameraScreen = observer(function QrCodeScannerCameraSc
   // const navigation = useNavigation()
   return (
     <Screen style={ROOT} preset="scroll">
-      <Text preset="header" text="" />
+      <Text preset="header" text="camera" />
     </Screen>
   )
 })
