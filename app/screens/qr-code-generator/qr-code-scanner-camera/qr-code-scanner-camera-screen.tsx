48c9d3b420fcb01def4553900fbf8aaf3da3a8dd
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-23 14:54:41 +0100

    DOCUMENTATION

--- app/screens/qr-code-generator/qr-code-scanner-camera/qr-code-scanner-camera-screen.tsx
+++ app/screens/qr-code-generator/qr-code-scanner-camera/qr-code-scanner-camera-screen.tsx
@@ -62,7 +62,7 @@ const saveQrToDisk=()=> {
       }
 
       />
-      <Button text="open url" style={{alignSelf:'center'}} onPress={()=>OpenQrCodeUrl("https://dev.to/aaronsm46722627/embedded-qr-code-scanner-and-browser-in-react-native-5752")} />
+      <Button text="open url" style={{alignSelf:'center'}} onPress={()=>OpenQrCodeUrl("https://www.youtube.com/watch?v=iJzJ7d5CK4A")} />
       
     </Screen>
   )
