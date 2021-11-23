3f61853da8d36c3ff452692e45dfe8038f83405e
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-23 11:38:52 +0100

    DOCUMENTATION

--- app/screens/qr-code-generator/qr-code-generator-screen.tsx
+++ app/screens/qr-code-generator/qr-code-generator-screen.tsx
@@ -87,7 +87,7 @@ export const QrCodeGeneratorScreen = observer(function QrCodeGeneratorScreen() {
       <Icon  name ="camera"  color="white" size={40}  />
 
       </TouchableOpacity>
-      <TouchableOpacity style={SCANNER_QR} onPress={()=>{callback(svg1);saveQrToDisk()}}>
+      <TouchableOpacity style={SCANNER_QR} onPress={()=>{saveQrToDisk()}}>
         <Text>Share Qr code</Text>
       <Icon  name ="share"  color="orange" size={40}  />
 
