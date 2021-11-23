2e23eeee07fd6ba8efd0c4f7249f8a8329754c04
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-23 11:24:58 +0100

    DOCUMENTATION

--- app/screens/qr-code-generator/qr-code-scanner-camera/qr-code-scanner-camera-screen.tsx
+++ app/screens/qr-code-generator/qr-code-scanner-camera/qr-code-scanner-camera-screen.tsx
@@ -1,12 +1,13 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle,TouchableOpacity,Linking } from "react-native"
+import { CameraRoll , ToastAndroid,Alert, Share ,TextInput, TouchableOpacity, View,Linking, ViewStyle } from "react-native"
 import { Screen, Text} from "../../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../../theme"
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import { RNCamera } from 'react-native-camera';
+//import { RNCamera } from 'react-native-camera';
+import RNFS from "react-native-fs"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
@@ -19,6 +20,22 @@ export const QrCodeScannerCameraScreen = observer(function QrCodeScannerCameraSc
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  let svg1= useRef("");
+
+
+const [imageSave,SetSave]=useState({ busy: false, imageSaved: true  })
+const saveQrToDisk=()=> {
+  svg1.toDataURL((data) => {
+    RNFS.writeFile(RNFS.CachesDirectoryPath+"/some-name.png", data, 'base64')
+      .then((success) => {
+        return CameraRoll.saveToCameraRoll(RNFS.CachesDirectoryPath+"/some-name.png", 'photo')
+      })
+      .then(() => {
+        SetSave({ busy: false, imageSaved: true  })
+        ToastAndroid.show('Saved to gallery !!', ToastAndroid.SHORT)
+      })
+  })
+}
   const Read_Qr_Code= e =>{
     Linking.openURL(e.data).catch(err=>
       console.error("An error occured ",err)
