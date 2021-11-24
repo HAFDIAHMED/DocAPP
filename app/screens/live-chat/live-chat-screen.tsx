a4945ef1ad766544c214a8c385807e297ee66ef3
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-24 10:45:17 +0100

    DOCUMENTATION

--- app/screens/live-chat/live-chat-screen.tsx
+++ app/screens/live-chat/live-chat-screen.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { TextStyle, ViewStyle } from "react-native"
+import { TextStyle, View, ViewStyle } from "react-native"
 import { Button, Screen, Text } from "../../components"
 import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
@@ -21,9 +21,12 @@ export const LiveChatScreen = observer(function LiveChatScreen() {
     <Screen style={ROOT} preset="scroll">
       <Text preset="header" text="live chat screen"   style= {TEXT_STYLE}/>
       <Button text="Live Message" style={Button_Message}  textStyle={TEXT_STYLE} onPress={()=>navigation.navigate("live_message")}/>
+      <View>
       <TouchableOpacity style={CHAT_BUTTON}>
         <Text style={TEXT_STYLE}>Live Message</Text>
       </TouchableOpacity>
+      </View>
+      
     </Screen>
   )
 })
