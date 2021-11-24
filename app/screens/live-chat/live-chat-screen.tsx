141b3a425517ea15f5236123d9b783849475f6b7
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-24 10:33:12 +0100

    DOCUMENTATION

--- app/screens/live-chat/live-chat-screen.tsx
+++ app/screens/live-chat/live-chat-screen.tsx
@@ -7,6 +7,7 @@ import { useNavigation } from "@react-navigation/native"
 import { color } from "../../theme"
 import metrics from "../../theme/metrics"
 import { FAB } from 'react-native-elements';
+import { TouchableOpacity } from "react-native-gesture-handler"
 
 
 
@@ -20,8 +21,9 @@ export const LiveChatScreen = observer(function LiveChatScreen() {
     <Screen style={ROOT} preset="scroll">
       <Text preset="header" text="live chat screen"   style= {TEXT_STYLE}/>
       <Button text="Live Message" style={Button_Message}  textStyle={TEXT_STYLE} onPress={()=>navigation.navigate("live_message")}/>
-      <FAB title="Create" />
-
+      <TouchableOpacity style={CHAT_BUTTON}>
+        <Text style={TEXT_STYLE}>Live Message</Text>
+      </TouchableOpacity>
     </Screen>
   )
 })
@@ -36,4 +38,8 @@ const TEXT_STYLE : TextStyle={
 const Button_Message : ViewStyle={
   alignSelf:'center',
   margin:metrics.widthPercentageToDP(2),
+}
+const CHAT_BUTTON :ViewStyle={
+  borderWidth:1,
+  borderRadius:20,
 }
