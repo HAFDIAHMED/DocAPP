cd19386cf4929ccf2887feeb46c86eb7ad1bc487
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-24 10:57:23 +0100

    DOCUMENTATION

--- app/screens/live-chat/live-chat-screen.tsx
+++ app/screens/live-chat/live-chat-screen.tsx
@@ -56,6 +56,15 @@ const CHAT_BUTTON :ViewStyle={
   justifyContent:'center',
   //position:'absolute',
  marginRight:metrics.widthPercentageToDP(4),
+ shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 9,
+},
+shadowOpacity: 0.48,
+shadowRadius: 11.95,
+
+elevation: 18,
  
  
 }
