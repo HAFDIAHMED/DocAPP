1d6adf2865fa8a19d4a0419e14f3373f8197520b
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-25 17:50:56 +0100

    DOCUMENTATION

--- app/screens/bottom-sheet-example/bottom-sheet-example-screen.tsx
+++ app/screens/bottom-sheet-example/bottom-sheet-example-screen.tsx
@@ -42,7 +42,7 @@ export const BottomSheetExampleScreen = observer(function BottomSheetExampleScre
   return (
     <Screen style={ROOT} preset="scroll">
       <Text preset="header" text="bottom sheet example" style={{alignSelf:'center'}} />
-      <Button text="open sheet" onPress={()=>setSheetIndex(0)} />
+      <Button text="open sheet" onPress={()=>setSheetIndex(2)} />
       <BottomSheet
         ref={bottomSheetRef}
         index={sheetIndex}
