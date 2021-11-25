eb178b5362877cb37a0714afef45f0253330326c
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-25 16:47:04 +0100

    DOCUMENTATION

--- app/screens/bottom-sheet-example/bottom-sheet-example-screen.tsx
+++ app/screens/bottom-sheet-example/bottom-sheet-example-screen.tsx
@@ -19,7 +19,7 @@ export const BottomSheetExampleScreen = observer(function BottomSheetExampleScre
   // const navigation = useNavigation()
   return (
     <Screen style={ROOT} preset="scroll">
-      <Text preset="header" text="" />
+      <Text preset="header" text="bottom sheet example" style={{alignSelf:'center'}} />
     </Screen>
   )
 })
