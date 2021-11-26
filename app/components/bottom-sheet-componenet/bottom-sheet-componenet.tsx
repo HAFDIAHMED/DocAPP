5a1f87b45b3028ff21dd0b28210612d201809dd2
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-26 09:38:10 +0100

    DOCUMENTATION

--- app/components/bottom-sheet-componenet/bottom-sheet-componenet.tsx
+++ app/components/bottom-sheet-componenet/bottom-sheet-componenet.tsx
@@ -4,6 +4,8 @@ import { observer } from "mobx-react-lite"
 import { color, typography } from "../../theme"
 import { Text } from "../"
 import { flatten } from "ramda"
+import BottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet';
+import { useCallback, useMemo, useRef } from "react"
 
 const CONTAINER: ViewStyle = {
   justifyContent: "center",
@@ -28,7 +30,29 @@ export interface BottomSheetComponenetProps {
 export const BottomSheetComponenet = observer(function BottomSheetComponenet(props: BottomSheetComponenetProps) {
   const { style } = props
   const styles = flatten([CONTAINER, style])
+  const bottomSheetRef = useRef<BottomSheet>(null);
 
+  // variables
+  const snapPoints = useMemo(() => ['1%','15%','25%', '50%','70%'], []);
+
+  // callbacks
+  const handleSnapPress = useCallback((index) => {
+    bottomSheetRef.current?.snapToIndex(index);
+  }, []);
+  const handleClosePress = useCallback(() => {
+    bottomSheetRef.current?.close();
+  }, []);
+  const renderBackdrop = useCallback(
+    props => (
+      <BottomSheetBackdrop
+        {...props}
+        disappearsOnIndex={1}
+        appearsOnIndex={2}
+      />
+    ),
+    []
+  );
+  
   return (
     <View style={styles}>
       <Text style={TEXT}>Hello</Text>
