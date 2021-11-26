03cbff558c1ae569dbbd359589df6999deda2420
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-26 10:42:04 +0100

    DOCUMENTATION

--- app/navigators/main-navigator.tsx
+++ app/navigators/main-navigator.tsx
@@ -1,2 +1,18 @@
 import React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
+import { Page1Screen, Page2Screen } from "../screens";
+
+const PagesStack= createStackNavigator<any>();
+export function PagesNavigator(){
+    return (
+        <PagesStack.Navigator
+        screenOptions={{
+            headerShown: true
+        }}
+        >
+            <PagesStack.Screen name="page_1" component={Page1Screen}/>
+            <PagesStack.Screen name="page_2" component={Page2Screen}/>
+
+        </PagesStack.Navigator>
+    );
+}
