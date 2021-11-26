e8cfa0ee798aecfa2e46f9f0cf120149492fc49c
HAFDIAHMED <ahafdi62@gmail.com>
2021-11-26 11:17:28 +0100

    DOCUMENTATION

--- app/screens/page-1/page-1-screen.tsx
+++ app/screens/page-1/page-1-screen.tsx
@@ -1,21 +1,21 @@
-import React from "react"
+import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
 import { Button, Screen, Text } from "../../components"
  import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
-//import { NavigationInjectedProps } from "react-navigation";
+import { NavigationInjectedProps } from "react-navigation";
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
 }
-/*export interface Page1Props extends NavigationInjectedProps<{}> {
+export interface Page1Props extends NavigationInjectedProps<{}> {
     
-} */
+} 
 
-export const Page1Screen = observer(function Page1Screen()  {
+export const Page1Screen :FC<Page1Props> = observer(function Page1Screen()  {
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
 
